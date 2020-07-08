c153b4c53703ab77cb42e6c0f870f43e1cce860d
zfima <zzfima@gmail.com>
2020-07-08 10:18:18 +0300

    parametrized test

--- test_Gereeter.py
+++ test_Gereeter.py
@@ -1,9 +1,31 @@
 import Greeter
+import pytest
 
 
 def test_ReturnHello():
     assert Greeter.ReturnHello() == 'Hello my Frined'
 
 
-def test_ReturnSpecificPersonHello():
+def test_ReturnSpecificPersonHello1():
     assert Greeter.ReturnSpecificPersonHello('misha') == 'Hello misha'
+
+
+def test_ReturnSpecificPersonHello2():
+    assert Greeter.ReturnSpecificPersonHello('alex') == 'Hello alex'
+
+
+def test_ReturnSpecificPersonHello3():
+    assert Greeter.ReturnSpecificPersonHello('Gogy') == 'Hello Gogy'
+
+# instead of 3 above functions:
+@pytest.mark.parametrize(
+    'input, expected_output',
+    [
+        ('Misha', 'Hello Misha'),
+        ('Alex', 'Hello Alex'),
+        ('Dov', 'Hello Dov'),
+        ('Zina', 'Hello Zina')
+    ]
+)
+def test_ReturnSpecificPersonHelloParametrizes(input, expected_output):
+    assert Greeter.ReturnSpecificPersonHello(input) == expected_output
